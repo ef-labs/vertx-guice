55f9a08527f81a1732833f5c9c8c44e669417d65
Dan O'Reilly <oreilldf@gmail.com>
2016-09-01 17:08:08 -0400

    Only warn about no binding module being found if no parent injector is defined.
    Signed-off-by: Dan O'Reilly <oreilldf@gmail.com>

---
+++ vertx-guice/src/test/java/com/englishtown/vertx/guice/integration/DummyVerticle.java
@@ -0,0 +1,17 @@
+/*
+ * This is the confidential unpublished intellectual property of EMC Corporation,
+ * and includes without limitation exclusive copyright and trade secret rights
+ * of EMC throughout the world.
+ */
+package com.englishtown.vertx.guice.integration;
+
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * Verticle with no dependencies inejected.
+ */
+public class DummyVerticle extends AbstractVerticle {
+
+  public DummyVerticle() {
+  }
+}
